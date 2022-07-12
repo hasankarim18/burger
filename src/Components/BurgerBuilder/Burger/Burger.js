c98c26891ea71e2f526ec331ef346f9091a24e5d
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-12 17:08:13 +0600

    Show ingredients according to state

--- src/Components/BurgerBuilder/Burger/Burger.js
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Ingredient from '../Ingredient/Ingredient'
 import classes from './Burger.module.css'
+
+
 const Burger = (props) => {
+
+    let IngredientArr = props.ingredients.map(item => {
+        let amountArr = [...Array(item.amount).keys()]
+        return amountArr.map(_ => {
+            return <Ingredient type={item.type} key={Math.random()} />
+        })
+    }).reduce((arr, element) => {
+        return arr.concat(element)
+    }, [])
+
+
     return (
         <div className={`${classes.burger} pt-5`} >
             <Ingredient type="top" />
-            <Ingredient type="salad" />
-            <Ingredient type="cheese" />
-            <Ingredient type="meat" />
+            {IngredientArr.length <= 0 ? <h3>Add Some ingredients </h3> : IngredientArr}
             <Ingredient type="bottom" />
 
         </div>
--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,18 @@ import Burger from './Burger/Burger'
 
 
 export class BurgerBuilder extends Component {
+    state = {
+        ingredients: [
+            { type: 'salad', amount: 1 },
+            { type: 'meat', amount: 1 },
+            { type: 'cheese', amount: 1 }
+        ]
+    }
     render() {
         return (
             <div className="row">
                 <div className="col-sm-6 col-12">
-                    <Burger />
+                    <Burger ingredients={this.state.ingredients} />
                 </div>
                 <div className="col-sm-6 col-12 ">
                     control
